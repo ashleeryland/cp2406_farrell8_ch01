6d0cffdd29a1642db44f5c3bed008c7a08fba32f
ashleeryland <rylandashlee@gmail.com>
2016-08-03 07:47:15 +1000

    Prac1

---
+++ DebuggingExercises/FavouriteSong.java
@@ -0,0 +1,10 @@
+public class FavouriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("You got a fast car");
+        System.out.println("I want a ticket to anywhere");
+        System.out.println("Maybe we make a deal");
+        System.out.println("Maybe together we can get somewhere");
+    }
+}
